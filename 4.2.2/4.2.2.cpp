86d74b93d44281649e213746c7485feda838edb9
Zhitikhina Mariia <zhitikhinamv@gmail.com>
2023-07-15 11:56:38 +0300

    first commit

---
+++ 4.2.2/4.2.2.cpp
@@ -0,0 +1,39 @@
+
+﻿//2.8.1
+#include <iostream>
+
+class Bad_length : public std::exception
+{
+public:
+    const char* what() const override { return "Вы ввели слово запретной длины! До свидания"; }
+};
+
+int func(std::string str, int forbidden_length) {
+    int length = str.length(); 
+    if (length == forbidden_length) throw Bad_length();
+    return length;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    int forbidden_length;
+    char length;//
+    char c = 34;
+    std::string str;
+    std::cout << "Введите запретную длину: ";
+    std::cin >> forbidden_length;
+    try
+    {
+        while (true) {
+            std::cout << "Введите слово: ";
+            std::cin >> str;
+            length = function(str, forbidden_length);//
+            std::cout << "Длина слова " << c << " " << str << " " << c << " равна " << length;
+            std::cout << std::endl;
+        }
+    }
+    catch (const Bad_length& ex) { std::cout << ex.what() << std::endl; }
+    catch (...) { std::cout << "Неизвестная ошибка" << std::endl; }
+}
